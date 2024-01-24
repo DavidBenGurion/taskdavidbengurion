3481b68fbbde3fcc05ecdf9420a74859a26ddda5
dabengurion <davidbengurion1999@gmail.com>
2024-01-25 02:06:43 +0700

    updatet terbaru

--- src/views/Main/HotTag.jsx
+++ src/views/Main/HotTag.jsx
@@ -5,9 +5,8 @@ const HotTag=(props)=>{
   return(
     <div className="bg-light mt-5 p-1 rounded shadow">
       <div>
-         <h3 id={styles.title_hot_tag}>Hot tag dalam satu minggu</h3>
-        {
-          props.hotTag.map((hotTag)=>(
+         <h3 id={styles.title_hot_tag}>Hot tag in a week </h3>
+          {props.hotTag.map((hotTag)=>(
             <BoxPhotoHotTag hotTag={hotTag}/>
           ))
         } 
